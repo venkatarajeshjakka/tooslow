89bd2f1bd1ef191953716da8a795ee1aa54292b2
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2019-12-15 15:53:19 -0500

    adding like button

--- src/screens/SearchResultScreen.js
+++ src/screens/SearchResultScreen.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import {
   View,
   Text,
   ActivityIndicator,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from "react-native";
 import { Context as StockContext } from "../context/StockContext";
 import { NavigationEvents } from "react-navigation";
@@ -12,9 +13,22 @@ import Section from "../components/Section";
 import Card from "../components/Card";
 import { formatCurrency, trimValue } from "../extension/Formatter";
 import TargetPriceCard from "../components/TargetPriceCard";
-
+import * as Animatable from "react-native-animatable";
+import { AntDesign } from "@expo/vector-icons";
 const SearchResultScreen = ({ navigation }) => {
+  
   const stockCode = navigation.getParam("stockCode");
+  const [liked, setLiked] = useState(false);
+  const AnimatedIcon = Animatable.createAnimatableComponent(AntDesign);
+
+  handleSmallAnimatedIconRef = ref => {
+    this.smallAnimatedIcon = ref;
+  };
+
+  handleOnPressLike = () => {
+    this.smallAnimatedIcon.bounceIn();
+    setLiked(!liked);
+  };
 
   const {
     state: { searchStockData },
@@ -64,11 +78,26 @@ const SearchResultScreen = ({ navigation }) => {
               <Text style={styles.heading}>{longName}</Text>
             </View>
             <Text style={{ padding: 5 }}>{regularMarketPrice}</Text>
-            <Text style={{ padding: 5 }}>
-              {`${trimValue(regularMarketChange)}(${trimValue(
-                regularMarketChangePercent * 100
-              )}%)`}
-            </Text>
+            <View
+              style={{
+                flexDirection: "row"
+              }}
+            >
+              <Text style={{ padding: 5, flex: 3 }}>
+                {`${trimValue(regularMarketChange)}(${trimValue(
+                  regularMarketChangePercent * 100
+                )}%)`}
+              </Text>
+              <TouchableOpacity onPress={handleOnPressLike}>
+                <AnimatedIcon
+                  ref={handleSmallAnimatedIconRef}
+                  name={liked ? "heart" : "hearto"}
+                  color={liked ? "#e92f3c": "#515151"}
+                  size={25}
+                  style={styles.icon}
+                />
+              </TouchableOpacity>
+            </View>
           </View>
           <Card>
             <Section>
@@ -150,6 +179,13 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginHorizontal: 15,
     alignContent: "stretch"
+  },
+  icon: {
+    paddingHorizontal: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "flex-end",
+    flex: 1
   }
 });
 
