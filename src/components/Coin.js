77258684f5cc9e9d6b24207d90374cd665d3ab79
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2019-12-28 14:59:34 -0500

    added swipeable card

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,58 +1,138 @@
-import React from 'react'
-import { View, Text,StyleSheet,TouchableOpacity } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated
+} from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import Change from "./Change";
 
-import Change from './Change'
-const Coin = ({onPress,symbol,price,name,change}) => {
-    return (
-        <TouchableOpacity onPress={onPress}>
-        <View style={styles.row}>
-          <Text style={styles.text} numberOfLines={1}>
-            {symbol}
-          </Text>
-          <View style={styles.right}>
-            <Text style={styles.text} numberOfLines={1}>
-              {price}
-            </Text>
-          </View>
-        </View>
-
-        <View style={styles.row}>
-          <Text style={[styles.text, styles.name]} numberOfLines={1}>
-            {name}
-          </Text>
-          <View style={styles.right}>
-            <Change value={change} />
-          </View>
-        </View>
-        </TouchableOpacity>
-       
-    )
-}
-const styles = StyleSheet.create({
-    container: {
-      borderRadius: 10,
-      padding: 10,
-      flex : 1
-    },
-   
-    row: {
-      flexDirection: 'row',
-      paddingBottom: 5,
-      
-    },
-    right: {
-      flex: 1,
-      alignSelf: 'flex-end',
-      alignItems: 'flex-end',
-    },
-    text: {
-      fontFamily: 'Avenir',
-      fontSize: 18,
-      fontWeight: '500',
-    },
-    name: {
-        fontSize: 16,
-      fontWeight: '300',
-    },
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: "clamp"
   });
-export default Coin
+  return (
+    <View style={styles.leftAction}>
+      <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+        Add to Portfolio
+      </Animated.Text>
+    </View>
+  );
+};
+
+const RightActions = ({ progress, dragX, onPress }) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: "clamp"
+  });
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.rightAction}>
+        <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+          Delete
+        </Animated.Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const Coin = ({
+  onSwipeFromLeft,
+  onRightPress,
+  onPress,
+  symbol,
+  price,
+  name,
+  change
+}) => {
+  return (
+    <Swipeable
+      renderLeftActions={LeftActions}
+      onSwipeableLeftOpen={onSwipeFromLeft}
+      renderRightActions={(progress, dragX) => (
+        <RightActions
+          progress={progress}
+          dragX={dragX}
+          onPress={onRightPress}
+        />
+      )}
+    >
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.text} numberOfLines={1}>
+              {symbol}
+            </Text>
+            <View style={styles.right}>
+              <Text style={styles.text} numberOfLines={1}>
+                {price}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <Text style={[styles.text, styles.name]} numberOfLines={1}>
+              {name}
+            </Text>
+            <View style={styles.right}>
+              <Change value={change} />
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Swipeable>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    margin: 5,
+    borderColor: "#ddd",
+    borderBottomWidth: 1,
+    backgroundColor: "#fff"
+  },
+
+  row: {
+    flexDirection: "row",
+    paddingBottom: 5
+  },
+  right: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "flex-end"
+  },
+  text: {
+    fontFamily: "Avenir",
+    fontSize: 18,
+    fontWeight: "500"
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "300"
+  },
+  leftAction: {
+    backgroundColor: "#388e3c",
+    justifyContent: "center",
+    flex: 1,
+    paddingVertical: 10,
+    marginVertical: 5
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "600",
+    padding: 20
+  },
+  rightAction: {
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    paddingVertical: 10,
+    marginVertical: 5,
+    alignItems: "flex-end"
+  }
+});
+export default Coin;
--- src/screens/WatchListScreen.js
+++ src/screens/WatchListScreen.js
@@ -1,53 +1,42 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet ,ScrollView} from "react-native";
+import { SafeAreaView, StyleSheet, ScrollView } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import Coin from "../components/Coin";
 const WatchListScreen = () => {
   return (
     <SafeAreaView>
-    <ScrollView>
-    <View style={styles.container}>
+      <ScrollView>
         <Coin
           onPress={() => {}}
+          onSwipeFromLeft={() => alert("swiped from left")}
+          onRightPress ={() => alert('press on right')}
           symbol="Reliance"
           name="reliance Industries"
           price="1546"
-          change= {-2.1}
+          change={-2.1}
         />
-      </View>
-      <View style={styles.container}>
+
         <Coin
           onPress={() => {}}
           symbol="SBI"
           name="SBIN"
           price="334"
-          change= {-0.3}
+          change={-0.3}
         />
-      </View>
-      <View style={styles.container}>
+
         <Coin
           onPress={() => {}}
           symbol="Yes Bank"
           name="Yes Bank"
           price="51"
-          change= {3.3}
+          change={3.3}
         />
-      </View>
-    
-    </ScrollView>
-      
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    margin: 5,
-    borderColor: "#ddd",
-    borderBottomWidth: 1
-  }
-});
+const styles = StyleSheet.create({});
 WatchListScreen.navigationOptions = {
   title: "Watchlist",
   tabBarIcon: ({ tintColor }) => (
