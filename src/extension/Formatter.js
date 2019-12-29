1f57b1e4e0f20a62770bbf583c955be6455fa85d
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2019-12-28 19:45:46 -0500

    added market capital and updated the formatter

--- src/extension/Formatter.js
+++ src/extension/Formatter.js
@@ -1,15 +1,24 @@
-import moment from 'moment-timezone'
+import moment from "moment-timezone";
 
-const formatCurrency = (input, currentFormatter) => {
-  return `${currentFormatter} ${input}`;
+const formatCurrency = (input) =>  new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+  maximumFractionDigits: 2
+}).format(input);
+
+const trimValue = input => Number(input).toFixed(2);
+
+
+const formatDate = input =>  moment(input)
+    .tz("Asia/Kolkata")
+    .format("lll");
+
+const formatMarketCapital = input => {
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 2
+  }).format(input);
 };
 
-const trimValue = input => {
-  return Number(input).toFixed(2);
-};
-
-const formatDate = input => {
-  return moment(input).tz('Asia/Kolkata').format('lll')
-}
-
-export { formatCurrency, trimValue ,formatDate };
+export { formatCurrency, trimValue, formatDate, formatMarketCapital };
