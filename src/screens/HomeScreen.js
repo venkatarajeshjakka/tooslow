5e6798704465172aa27c17d5dd1e62f503cfe8c5
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-01-04 16:12:33 -0500

    added chart

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { View, Text ,SafeAreaView, Button} from 'react-native'
-
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import { Defs, LinearGradient, Stop } from 'react-native-svg'
 const HomeScreen = () => {
+    const data = [ 50, 10, 40, 95, 85, 91, 35, 53,  24, 50 ]
+
+    const Gradient = ({ index }) => (
+        <Defs key={index}>
+            <LinearGradient id={'gradient'} x1={'0%'} y1={'0%'} x2={'0%'} y2={'100%'}>
+                <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.8}/>
+                <Stop offset={'100%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.2}/>
+            </LinearGradient>
+        </Defs>
+    )
     return (
         <SafeAreaView>
-            <View>
-                 <Text>Hello</Text>
-             </View>
+        <AreaChart
+                style={{ height: 200 }}
+                data={data}
+                contentInset={{ top: 20, bottom: 20 }}
+                svg={{ fill: 'url(#gradient)' }}
+            >
+                
+                <Gradient/>
+            </AreaChart>
         </SafeAreaView>
         
     )
