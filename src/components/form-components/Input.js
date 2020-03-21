e1bb217035815f745cdeb25aa3de9be8bdafe594
Venkatarajesh Jakka <venkatarajeshjakka@Venkatarajeshs-MacBook-Air.local>
2020-03-21 01:03:32 -0400

    styling of input

--- src/components/form-components/Input.js
+++ src/components/form-components/Input.js
@@ -29,13 +29,14 @@ const Input = ({
 };
 const styles = StyleSheet.create({
   labelStyle: {
-    fontSize: 18,
-    flex: 1
+    fontSize: 16,
+    flex: 1,
+    paddingLeft :5,
+    fontFamily: 'Avenir',
   },
   inputStyle: {
     color: "#000",
     paddingRight: 5,
-    paddingLeft: 5,
     fontSize: 16,
     lineHeight: 23,
     flex: 2,
@@ -45,19 +46,19 @@ const styles = StyleSheet.create({
   containerStyle: {
     flex: 1,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    
   },
   field: {
-    paddingHorizontal: 5,
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderColor: "#6a0dad",
+    padding :10,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#0A84FF",
     backgroundColor: "#fff",
     justifyContent: "flex-start",
     flexDirection: "row",
     position: "relative",
-    marginHorizontal: 15,
-    marginTop: 20
+    margin: 15,
   }
 });
 export { Input };
